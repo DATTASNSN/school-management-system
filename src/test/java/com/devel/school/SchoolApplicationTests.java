04cbd2ba96a75e5d2e7bafde9501587955a7fb75
Samayamanthula Navya Sai Narayana Datta <samayamantula.saidatta@gmail.com>
2024-12-07 17:44:13 +0530

    Initial Commit

---
+++ src/test/java/com/devel/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devel.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
